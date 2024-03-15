20259eb998ac19c9cdc58ff218f435c92ee28a52
ThaiToni <vietthaist01@gmail.com>
2024-03-16 01:26:21 +0700

    add crud

---
+++ Schedule/Pages/Room/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Schedule.Models;
+
+namespace Schedule.Pages.Room
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Schedule.Models.ScheduleManagementContext _context;
+
+        public DetailsModel(Schedule.Models.ScheduleManagementContext context)
+        {
+            _context = context;
+        }
+
+      public Models.Room Room { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Rooms == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Rooms.FirstOrDefaultAsync(m => m.Id == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Room = room;
+            }
+            return Page();
+        }
+    }
+}
---
+++ Schedule/Pages/Subject/Details.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Schedule.Models;
+
+namespace Schedule.Pages.Subject
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Schedule.Models.ScheduleManagementContext _context;
+
+        public DetailsModel(Schedule.Models.ScheduleManagementContext context)
+        {
+            _context = context;
+        }
+
+      public Models.Subject Subject { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Subjects == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects.FirstOrDefaultAsync(m => m.Id == id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Subject = subject;
+            }
+            return Page();
+        }
+    }
+}
